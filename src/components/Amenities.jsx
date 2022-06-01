57308323adf32b50b3872c51cc70a4228a8d4022
KyleBPerez <star_wars51389@yahoo.com>
2022-06-01 15:17:25 -0700

    removed react fragment
    made margin top and bottom larger

--- src/components/Amenities.jsx
+++ src/components/Amenities.jsx
@@ -1,105 +1,103 @@
 export default function Amenities() {
   return (
-    <>
-      <div className='d-flex flex-column'>
+    <div className='d-flex flex-column my-5'>
+      <div>
+        <h2>Amenities</h2>
+        <h3>Everyday</h3>
+      </div>
+      <div className='d-flex flex-wrap text-center'>
         <div>
-          <h2>Amenities</h2>
-          <h3>Everyday</h3>
+          <img
+            alt='parking'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/parking.png?im_w=240'
+          />
+          <div>Free parking on premises</div>
         </div>
-        <div className='d-flex flex-wrap text-center'>
-          <div>
-            <img
-              alt='parking'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/parking.png?im_w=240'
-            />
-            <div>Free parking on premises</div>
-          </div>
-          <div>
-            <img
-              alt='kitchenette'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/kitchen.png?im_w=240'
-            />
-            <div>Kitchenette</div>
-          </div>
-          <div>
-            <img
-              alt='wifi'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/wireless-internet.png?im_w=240'
-            />
-            <div>Wifi</div>
-          </div>
-          <div>
-            <img
-              alt='tv'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/tv.png?im_w=240'
-            />
-            <div>TV</div>
-          </div>
-          <div>
-            <img
-              alt='bathroom essentials'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/essentials.png?im_w=240'
-            />
-            <div>Bathroom essentials</div>
-          </div>
-          <div>
-            <img
-              alt='bedroom comforts'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/extra-pillows-blankets.png?im_w=240'
-            />
-            <div>Bedroom comforts</div>
-          </div>
-          <div>
-            <img
-              alt='coffee maker'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/coffee-maker.png?im_w=240'
-            />
-            <div>Coffee maker</div>
-          </div>
-          <div>
-            <img
-              alt='hair dryer'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/hair-dryer.png?im_w=240'
-            />
-            <div>Hair dryer</div>
-          </div>
-          <div>
-            <img
-              alt='heating'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/heating.png?im_w=240'
-            />
-            <div>Heating</div>
-          </div>
-          <div>
-            <img
-              alt='ac'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/air-conditioning.png?im_w=240'
-            />
-            <div>Air conditioning</div>
-          </div>
-          <div>
-            <img
-              alt='backyard'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/garden-backyard.png?im_w=240'
-            />
-            <div>Backyard</div>
-          </div>
-          <div>
-            <img
-              alt='smoke alarm'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/smoke-detector.png?im_w=240'
-            />
-            <div>Smoke alarm</div>
-          </div>
-          <div>
-            <img
-              alt='carbon monoxide alarm'
-              src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/co2-detector.png?im_w=240'
-            />
-            <div>Carbon monoxide alarm</div>
-          </div>
+        <div>
+          <img
+            alt='kitchenette'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/kitchen.png?im_w=240'
+          />
+          <div>Kitchenette</div>
+        </div>
+        <div>
+          <img
+            alt='wifi'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/wireless-internet.png?im_w=240'
+          />
+          <div>Wifi</div>
+        </div>
+        <div>
+          <img
+            alt='tv'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/tv.png?im_w=240'
+          />
+          <div>TV</div>
+        </div>
+        <div>
+          <img
+            alt='bathroom essentials'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/essentials.png?im_w=240'
+          />
+          <div>Bathroom essentials</div>
+        </div>
+        <div>
+          <img
+            alt='bedroom comforts'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/extra-pillows-blankets.png?im_w=240'
+          />
+          <div>Bedroom comforts</div>
+        </div>
+        <div>
+          <img
+            alt='coffee maker'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/coffee-maker.png?im_w=240'
+          />
+          <div>Coffee maker</div>
+        </div>
+        <div>
+          <img
+            alt='hair dryer'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/hair-dryer.png?im_w=240'
+          />
+          <div>Hair dryer</div>
+        </div>
+        <div>
+          <img
+            alt='heating'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/tile-view-2x/heating.png?im_w=240'
+          />
+          <div>Heating</div>
+        </div>
+        <div>
+          <img
+            alt='ac'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/air-conditioning.png?im_w=240'
+          />
+          <div>Air conditioning</div>
+        </div>
+        <div>
+          <img
+            alt='backyard'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/garden-backyard.png?im_w=240'
+          />
+          <div>Backyard</div>
+        </div>
+        <div>
+          <img
+            alt='smoke alarm'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/smoke-detector.png?im_w=240'
+          />
+          <div>Smoke alarm</div>
+        </div>
+        <div>
+          <img
+            alt='carbon monoxide alarm'
+            src='https://a0.muscache.com/airbnb/static/select/pdp/amenities/list-view-2x/co2-detector.png?im_w=240'
+          />
+          <div>Carbon monoxide alarm</div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
