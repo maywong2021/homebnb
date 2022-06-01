c2c96289869c9a4aff1f0d7c9422bd72de79a77e
KyleBPerez <star_wars51389@yahoo.com>
2022-06-01 15:15:45 -0700

    added styling to ThingToKnow

--- src/components/ThingsToKnow.jsx
+++ src/components/ThingsToKnow.jsx
@@ -2,73 +2,83 @@ import {
   AiFillClockCircle,
   AiFillExclamationCircle,
   AiFillCreditCard,
-  AiFillSound
-} from "react-icons/ai";
-import { IoLogoNoSmoking } from "react-icons/io";
-import { MdPets } from "react-icons/md";
-import { BiAlarmExclamation } from "react-icons/bi";
-import { GiStairs, GiBalloons } from "react-icons/gi";
-import { HiSparkles } from "react-icons/hi";
+  AiFillSound,
+} from 'react-icons/ai'
+import { IoLogoNoSmoking } from 'react-icons/io'
+import { MdPets } from 'react-icons/md'
+import { BiAlarmExclamation } from 'react-icons/bi'
+import { GiStairs, GiBalloons } from 'react-icons/gi'
+import { HiSparkles } from 'react-icons/hi'
 
 export default function ThingsToKnow() {
   return (
-    <>
-      <hr />
-      <div>
-        <h2>Things to know</h2>
-        <h3>House rules</h3>
-        <div>
-          <ol>
+    <div className='d-flex flex-column my-5'>
+      <h3 className='mb-4'>Things to know</h3>
+      <div className='d-flex justify-content-between'>
+        <div style={{ width: '30%' }}>
+          <h5>House rules</h5>
+          <div>
+            <ol style={{ listStyle: 'none' }}>
+              <li>
+                <AiFillClockCircle size={15} />
+                Check-in: 3:00 PM - 12:00 AM
+              </li>
+              <li>
+                <AiFillClockCircle size={15} />
+                Checkout: 10:00 AM
+              </li>
+              <li>
+                <IoLogoNoSmoking size={15} />
+                No smoking
+              </li>
+              <li>
+                <MdPets size={15} />
+                Pets allowed
+              </li>
+              <li>
+                <GiBalloons size={15} />
+                Parties and events allowed
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className='mx-4' style={{ width: '30%' }}>
+          <h5>Health & safety</h5>
+          <div>
+            <ol style={{ listStyle: 'none' }}>
+              <li>
+                <HiSparkles size={15} />
+                Homebnb's COVID-19 safety practices apply
+              </li>
+              <li>
+                <AiFillExclamationCircle size={15} />
+                Security camera/recording device
+              </li>
+              <li>
+                <BiAlarmExclamation size={15} />
+                Carbon monoxide alarm
+              </li>
+              <li>
+                <BiAlarmExclamation size={15} />
+                Smoke alarm
+              </li>
+              <li>
+                <AiFillCreditCard size={15} />
+                Security Deposit - if you damage the home, you may be charged up
+                to $350
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div style={{ width: '30%' }}>
+          <h5>Cancellation policy</h5>
+          <ol style={{ listStyle: 'none' }}>
             <li>
-              <AiFillClockCircle size={15} />
-              Check-in: 3:00 PM - 12:00 AM
-            </li>
-            <li>
-              <AiFillClockCircle size={15} />
-              Checkout: 10:00 AM
-            </li>
-            <li>
-              <IoLogoNoSmoking size={15} />
-              No smoking
-            </li>
-            <li>
-              <MdPets size={15} />
-              Pets allowed
-            </li>
-            <li>
-              <GiBalloons size={15} />
-              Parties and events allowed
+              Add your trip dates to get the cancellation details for this stay.
             </li>
           </ol>
         </div>
-        <h3>Health & safety</h3>
-        <div>
-          <ol>
-            <li>
-              <HiSparkles size={15} />
-              Homebnb's COVID-19 safety practices apply
-            </li>
-            <li>
-              <AiFillExclamationCircle size={15} />
-              Security camera/recording device
-            </li>
-            <li>
-              <BiAlarmExclamation size={15} />
-              Carbon monoxide alarm
-            </li>
-            <li>
-              <BiAlarmExclamation size={15} />
-              Smoke alarm
-            </li>
-            <li>
-              <AiFillCreditCard size={15} />
-              Security Deposit - if you damage the home, you may be charged up
-              to $350
-            </li>
-          </ol>
-        </div>
-        <h3>Cancellation policy</h3>
       </div>
-    </>
-  );
+    </div>
+  )
 }
