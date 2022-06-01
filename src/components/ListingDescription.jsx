dd1e80722a054fb8178b40996a526ba3e1de45a9
KyleBPerez <star_wars51389@yahoo.com>
2022-06-01 14:32:42 -0700

    added listing prop
    plugged in props to support a more dynamic site
    
    changed the style on the button icon to be round and used the props to use the hosts userIcon
    
    added description and if any addition notes where added to the listing

--- src/components/ListingDescription.jsx
+++ src/components/ListingDescription.jsx
@@ -1,59 +1,46 @@
-import Button from "react-bootstrap/Button";
-import { BsDot } from "react-icons/bs";
+import Button from 'react-bootstrap/Button'
+import { BsDot } from 'react-icons/bs'
 
-export default function ListingDescription() {
+export default function ListingDescription({
+  listing: { host, arrangement, description },
+}) {
   return (
-    <div id="listing-desc">
-        <div className="d-flex justify-content-between align-items-center">
-          <div>
-            <h3>Treehouse hosted by The Cyberknights</h3>
-            <ol id="room-list" className="d-flex">
-              <li>
-                <span>4 guests</span>
-              </li>
-              <li>
-                <span>
-                  <BsDot size={10} />
-                </span>
-                <span>1 bedroom</span>
-              </li>
-              <li>
-                <span>
-                  <BsDot size={10} />
-                </span>
-                <span>1 bed</span>
-              </li>
-              <li>
-                <span>
-                  <BsDot size={10} />
-                </span>
-                <span>1 bath</span>
-              </li>
-            </ol>
-          </div>
-          <Button>
-            <img
-              alt="profile"
-              src="https://cdn-icons-png.flaticon.com/512/63/63633.png"
-              height="40px"
-              width="40px"
-            ></img>
-          </Button>
+    <div id='listing-desc'>
+      <div className='d-flex justify-content-between align-items-center'>
+        <div>
+          <h3>{`Treehouse hosted by ${host?.username}`}</h3>
+          <ol id='room-list' className='d-flex'>
+            {arrangement?.map(({ name, count }, i) => {
+              return i === 0 ? (
+                <li key={i}>
+                  <span>{`${count} ${name}`}</span>
+                </li>
+              ) : (
+                <li key={i}>
+                  <BsDot />
+                  <span>{`${count} ${name}`}</span>
+                </li>
+              )
+            })}
+          </ol>
         </div>
-        <hr />
-        <div className="py-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+        <Button className='p-0' style={{ borderRadius: '50%' }}>
+          <img
+            alt='profile'
+            src={host?.userIcon}
+            style={{
+              width: '50px',
+              height: '50px',
+              borderRadius: '50%',
+            }}
+          />
+        </Button>
+      </div>
+      <hr />
+      <div className='py-4'>
+        <p>{description?.main}</p>
+        {description?.notes && <p>{description?.notes}</p>}
+      </div>
     </div>
-  );
+  )
 }
