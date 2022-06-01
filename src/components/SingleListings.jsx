70669bed05c40ce23ad46615d56d7a9279564b0a
KyleBPerez <star_wars51389@yahoo.com>
2022-06-01 13:26:32 -0700

    added getListing call
    added useEffect to grab the current listing
    
    added useState to store the listing

--- src/components/SingleListings.jsx
+++ src/components/SingleListings.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import {
   ListingDescription,
   ListingHeading,
@@ -6,11 +7,23 @@ import {
   Location,
   ThingsToKnow,
 } from './index'
+import { getListing } from '../axios-services'
 
 export default function SingleListings() {
+  const [listing, setListing] = useState({})
+
+  useEffect(() => {
+    //? when more pages are set up replace the 1 in 'getListing(1)' with
+    //? the useParams hook to grab differenct listings
+    //? E.g. if url = home.bnb/listing/:listingId || const { listingId } = useParams
+    //?  const currListing = getListing(listingId) || this will make the code more dynamic
+    const currListing = getListing(1)
+    setListing(currListing)
+  }, [])
+
   return (
     <div className='d-flex flex-column justify-content-center w-75'>
-      <ListingHeading />
+      <ListingHeading listing={listing} />
       <div
         className='d-flex justify-content-center align-items-center'
         style={{ minWidth: '500px', minHeight: '400px' }}
@@ -18,7 +31,7 @@ export default function SingleListings() {
         <h1>Listing Images</h1>
       </div>
       <div className='d-flex justify-content-between'>
-        <ListingDescription />
+        <ListingDescription listing={listing} />
         <div className='w-25'>CALENDAR HERE</div>
       </div>
       <hr />
