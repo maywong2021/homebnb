68db2f0058d3e2031cffe638d24606666ed76a22
KyleBPerez <star_wars51389@yahoo.com>
2022-06-01 13:25:55 -0700

    added props for ListingHeading

--- src/components/ListingHeading.jsx
+++ src/components/ListingHeading.jsx
@@ -5,11 +5,13 @@ import { FiShare } from 'react-icons/fi'
 import { AiOutlineHeart } from 'react-icons/ai'
 import Button from 'react-bootstrap/Button'
 
-export default function ListingHeading() {
+export default function ListingHeading({
+  listing: { title, reviews, location },
+}) {
   return (
     <>
       <div>
-        <h1>The Treehouse of your dreams</h1>
+        <h1>{title}</h1>
       </div>
       <div className='d-flex justify-content-between align-content-center'>
         <div className='d-flex align-items-center gap-5'>
@@ -17,7 +19,7 @@ export default function ListingHeading() {
           <p className='align-self-center mb-0'>4.99</p>
           <BsDot size={10} />
           <Button variant='link' className='text-dark'>
-            423 reviews
+            {reviews.length} reviews
           </Button>
           <BsDot size={10} />
           <GoPerson id='go-person' />
@@ -25,7 +27,7 @@ export default function ListingHeading() {
           <BsDot size={10} />
           <span className='d-flex'>
             <Button variant='link' className='text-dark' value='Ashenville,'>
-              Asheville, North Carolina, United States
+              {location.where}
             </Button>
           </span>
         </div>
